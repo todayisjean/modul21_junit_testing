db6e5dd79f37f6fa95a77f82750b4fe779ee5d30
todayisjean <s31230057@student.ubm.ac.id>
2025-07-04 16:25:59 +0700

    Ubah opsi selenium ke headless

--- src/test/java/util/DriverManager.java
+++ src/test/java/util/DriverManager.java
@@ -2,13 +2,17 @@ package util;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 
 public class DriverManager {
     private static WebDriver _driver;
 
     public static WebDriver getDriver() {
-        if(_driver == null)
-            _driver = new ChromeDriver();
+        if(_driver == null) {
+            var option = new ChromeOptions();
+            option.addArguments("headless");
+            _driver = new ChromeDriver(option);
+        }
         return _driver;
     }
 
